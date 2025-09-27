4080d5a474b9c171a8259550b5c198f3e3003b95
Peter Byfield <peter.byfield@kraken.tech>
2025-09-27 07:33:04 +0200

    Create dynamic prompt

--- create_article_prompt.py
+++ create_article_prompt.py
@@ -1,3 +1,49 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "pydantic",
+# ]
+# ///
+
+
+import json
+
+import pydantic
+
+
+class ArticleSentence(pydantic.BaseModel):
+    text: str
+    translated_text: str
+
+
+class ArticleKeyVocab(pydantic.BaseModel):
+    term: str
+    translated_term: str
+    example_usage: str
+
+
+class ArticleTag(pydantic.BaseModel):
+    name: str
+    translated_name: str
+
+
+class ArticleSource(pydantic.BaseModel):
+    name: str
+    link_url: str
+
+
+class Article(pydantic.BaseModel):
+    title: str
+    translated_title: str
+    text: list[ArticleSentence]
+    key_vocab: list[ArticleKeyVocab]
+    tags: list[ArticleTag]
+    sources: list[ArticleSource]
+
+
+article_schema = Article.model_json_schema()
+
+prompt = f"""\
 Search the internet for today's top news.
 
 I'm especially interested in:
@@ -23,6 +69,14 @@ For each article:
 * Provide links to the original source articles, where one could go to read more.
   * Make sure these links point to specific articles, not generic sites.
 
-For each article, create a JSON file for the result under `articles/$today`, where `$today` is today's date (ISO format). Each JSON file should obey the schema given in `article-schema.json`.
+For each article, create a JSON file for the result under `articles/$today`, where `$today` is today's date (ISO format).
+Each JSON file should obey the schema given below:
+
+```json
+{json.dumps(article_schema, indent=2)}
+```
 
 Make your changes on a new branch. Commit your changes and open a PR. Assign Peter554 to review the PR.
+"""
+
+print(prompt)
